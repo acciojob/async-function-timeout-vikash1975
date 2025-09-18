e16bae376a0ae00c0d269ca5c444f66a4bc9ebdb
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2025-09-18 12:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+let textInput=document.getElementById("text");
+let delayInput=document.getElementById("delay");
+let btn=document.getElementById("btn");
+
+let output =document.createElement("div");
+output.id="output";
+output.style.marginTop="15px";
+output.style.fontSize="19px";
+
+document.body.appendChild(output);
+
+
+
+function wait(ms) {
+	return new Promise(resolve=>setTimeout(resolve,ms));
+}
+
+async function showMessage(){
+	const message=textInput.value;
+	const delay=Number(delayInput.value);
+	output.textContent="Waiting...";
+
+	await wait(delay);
+	output.textContent=message;
+}
+
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	showMessage();
+});
